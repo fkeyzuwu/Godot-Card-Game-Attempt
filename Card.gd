2bbbe3c5b773fe2298ee827b3d2a1094c2f27ae1
fkeyz <fkeyzuwu@gmail.com>
2022-09-04 21:02:55 +0300

    ok

--- Card.gd
+++ Card.gd
@@ -1,9 +1,10 @@
-tool
 extends Node2D
 class_name Card
 
 export(Resource) var data = data as CardData
-onready var nameText: Label = $"%Label"
+onready var nameText: Label = $"%Name"
+onready var descriptionText: Label = $"%Description"
 
 func _ready() -> void:
 	nameText.text = data.name
+	descriptionText.text = data.description
