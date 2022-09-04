852cda6f1cecb5eaf3e6d975cf9af38e4d0a156f
fkeyz <fkeyzuwu@gmail.com>
2022-09-04 21:39:33 +0300

    badoo

--- Card.gd
+++ Card.gd
@@ -2,9 +2,19 @@ extends Node2D
 class_name Card
 
 export(Resource) var data = data as CardData
+
+var card_name: String
+var card_description: String
+
 onready var nameText: Label = $"%Name"
 onready var descriptionText: Label = $"%Description"
+onready var background: ColorRect = $"%Background"
 
 func _ready() -> void:
-	nameText.text = data.name
-	descriptionText.text = data.description
+	card_name = data.name
+	card_description = data.description
+	
+	nameText.text = card_name
+	descriptionText.text = card_description
+	background.color = data.backgroundColor
+	
--- CardData.gd
+++ CardData.gd
@@ -3,3 +3,4 @@ class_name CardData
 
 export var name: String
 export var description: String
+export var backgroundColor: Color = Color(0.741176, 0.933333, 0.894118)
