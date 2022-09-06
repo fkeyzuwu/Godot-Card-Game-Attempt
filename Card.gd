b8f862e7916d711c7515b7f491d58ec2536b195d
fkeyz <fkeyzuwu@gmail.com>
2022-09-06 18:36:18 +0300

    added some stuff

--- Card.gd
+++ Card.gd
@@ -8,12 +8,13 @@ var card_description: String
 
 onready var nameText: Label = $"%Name"
 onready var descriptionText: Label = $"%Description"
+onready var texture: TextureRect = $"%TextureRect"
 onready var background: ColorRect = $"%Background"
 
 func _ready() -> void:
 	card_name = data.name
 	card_description = data.description
-	
+	texture.texture = data.sprite
 	nameText.text = card_name
 	descriptionText.text = card_description
 	background.color = data.backgroundColor
--- CardData.gd
+++ CardData.gd
@@ -3,4 +3,5 @@ class_name CardData
 
 export var name: String
 export var description: String
+export var sprite: Texture
 export var backgroundColor: Color = Color(0.741176, 0.933333, 0.894118)
