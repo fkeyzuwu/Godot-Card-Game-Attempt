f6a1e44a8da5cf562098314ac0df9f3f6424af4e
fkeyz <fkeyzuwu@gmail.com>
2022-09-09 22:35:07 +0300

    cool

--- Grid.gd
+++ Grid.gd
@@ -3,11 +3,20 @@ extends GridContainer
 func _ready() -> void:
 	add_constant_override("hseparation",-287)
 	
-	var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	
-	tween.tween_property(self, "rect_scale", 
-	Vector2.ONE, 1).from(Vector2.ZERO)
+	yield(get_tree().create_timer(0.2), "timeout")
+
+	var tween := create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.set_parallel().tween_property(self, "rect_scale", 
+	Vector2.ONE, 0.5).from(Vector2.ZERO)
+
+	tween.tween_property(self, "rect_position", 
+	rect_position, 0.5).from(Vector2(0, rect_position.y + rect_size.y * 0.5))
 	
 	tween.tween_property(self, "custom_constants/hseparation",
-	 21, 1)
+	 21, 2).set_trans(Tween.TRANS_QUINT)
+	tween.set_parallel(false)
+	tween.tween_callback(self, "suck")
+	
+func suck() -> void:
+	print("hi")
 	
