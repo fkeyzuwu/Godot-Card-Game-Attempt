13b39e8e48ea11c5ccc79b6c1b0b426bd0fb0fdf
fkeyz <fkeyzuwu@gmail.com>
2022-09-09 18:44:49 +0300

    i have nmo idea what im doing

--- Grid.gd
+++ Grid.gd
@@ -1,7 +1,13 @@
 extends GridContainer
 
-
 func _ready() -> void:
-	add_constant_override("hseparation", -1080)
-	var tween := create_tween().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "custom_constants/hseparation", 21, 1)
+	add_constant_override("hseparation",-287)
+	
+	var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	
+	tween.tween_property(self, "rect_scale", 
+	Vector2.ONE, 1).from(Vector2.ZERO)
+	
+	tween.tween_property(self, "custom_constants/hseparation",
+	 21, 1)
+	
